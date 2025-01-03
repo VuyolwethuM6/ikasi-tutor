870b4f8b6772cacc994db0026b5e6bfe9c3899bf
VuyolwethuM6 <vmabhuleka@gmail.com>
2025-01-03 14:58:20 +0200

    Added a hero image.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className="relative min-h-screen pt-16"> {/* Added pt-16 for navbar spacing */}
       <div className="absolute inset-0">
         <img
-          src="https://images.unsplash.com/photo-1577896851231-70ef18881754?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+          src="/public/hero.jpg"
           alt="Students learning"
           className="w-full h-full object-cover"
         />
