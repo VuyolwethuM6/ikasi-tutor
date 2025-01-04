9aca601620dcb7ed31799bbda323236026716042
VuyolwethuM6 <vmabhuleka@gmail.com>
2025-01-04 21:53:54 +0200

    Added an about image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div>
             <img
-              src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
+              src="/class.jpg"
               alt="Students studying"
               className="rounded-lg shadow-lg"
             />
